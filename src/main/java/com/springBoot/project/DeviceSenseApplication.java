2f1edca03785e431852adf336e48d2daea736396
TheEdu <edigde@gmail.com>
2017-06-03 18:16:11 -0300

    initial commit

---
+++ src/main/java/com/springBoot/project/DeviceSenseApplication.java
@@ -0,0 +1,12 @@
+package com.springBoot.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceSenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceSenseApplication.class, args);
+	}
+}
---
+++ src/main/java/com/springBoot/project/controller/queryController.java
@@ -0,0 +1,53 @@
+package com.springBoot.project.controller;
+
+
+import org.kairosdb.client.HttpClient;
+import org.kairosdb.client.builder.AggregatorFactory;
+import org.kairosdb.client.builder.QueryBuilder;
+import org.kairosdb.client.builder.TimeUnit;
+import org.kairosdb.client.response.GetResponse;
+import org.kairosdb.client.response.QueryResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class queryController {
+	
+    @RequestMapping("/metriNames")
+	public GetResponse getMetricNames(){
+    	GetResponse response = null;
+    	try{
+	    	HttpClient client = new HttpClient("http://localhost:8080");
+	    	response = client.getMetricNames();
+	
+	    	System.out.println("Response Code =" + response.getStatusCode());
+	    	for (String name : response.getResults()){
+	    		System.out.println(name);
+	    	}
+	    	
+	    	client.shutdown();
+    	} catch(Exception e){
+    		System.out.println("Error: " + e.getMessage());
+    	}
+		return response;
+	}
+    
+    @RequestMapping("/queryMetric")
+	public QueryResponse getMetricValues(){
+    	QueryResponse response = null;
+    	QueryBuilder builder = QueryBuilder.getInstance();
+    	builder.setStart(5, TimeUnit.HOURS)
+    	       .addMetric("kairosdb.datastore.query_time")
+    	       .addAggregator(AggregatorFactory.createSumAggregator(1, TimeUnit.MILLISECONDS));
+    	
+    	try{
+    		HttpClient client = new HttpClient("http://localhost:8080");
+    		response = client.query(builder);
+    		client.shutdown();
+    	} catch (Exception e){
+    		System.out.println("Error: " + e.getMessage());
+    	}
+		return response;
+	}
+    
+}
