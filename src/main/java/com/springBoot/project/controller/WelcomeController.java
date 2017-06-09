b91c4a83050d5dd84fe921be3f3b3756efe678a9
TheEdu <edigde@gmail.com>
2017-06-09 19:07:52 -0300

    metric button

--- src/main/java/com/springBoot/project/controller/WelcomeController.java
+++ src/main/java/com/springBoot/project/controller/WelcomeController.java
@@ -1,5 +1,9 @@
 package com.springBoot.project.controller;
 
+import java.util.List;
+
+import org.kairosdb.client.HttpClient;
+import org.kairosdb.client.response.GetResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +14,25 @@ public class WelcomeController {
 	@RequestMapping("/")
 	public String Welcome(Model model){
 		model.addAttribute("message", "Hola Mundo");
+		
+		GetResponse response = null;
+    	try{
+	    	HttpClient client = new HttpClient("http://localhost:8080");
+	    	response = client.getMetricNames();
+	
+	    	System.out.println("Response Code =" + response.getStatusCode());
+	    	for (String name : response.getResults()){
+	    		System.out.println(name);
+	    	}
+	    	
+	    	client.shutdown();
+    	} catch(Exception e){
+    		System.out.println("Error: " + e.getMessage());
+    	}
+    	List<String> metricNames = response.getResults();
+    	
+    	model.addAttribute("metricNames",metricNames);
+			
 		return "welcome";
 	}
 
--- src/main/webapp/view/welcome.jsp
+++ src/main/webapp/view/welcome.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -9,8 +10,20 @@
 </head>
 <body>
 	<div class="container-fluid">
-		<h1>Device Sense</h1>
-		<h4>${message}</h4>
+		<div>
+			<h1>Device Sense</h1>
+		</div>
+		<div>
+			<form action="#" method="post">
+				<label for="metric-names">Seleccione una Metrica:</label>
+				<select class="form-control" name='metric-name'>
+				    <c:forEach items="${metricNames}" var="metricName">
+				       <option value="${metricName}">${metricName}</option>
+				    </c:forEach>
+				</select>
+				<input type="submit" value="Consultar Metrica"> falta implementar xD
+			</form>
+		</div>
 	</div>	
 <script type="text/javascript" src="webjars/jquery/3.2.1/jquery.min.js"></script>
 </body>
